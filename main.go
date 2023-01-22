c170182a5d9e3f6749b5669f8456f3f9823874de
Aelpxy <aelpxy@protonmail.com>
2023-01-22 17:47:36 +0000

    feat: added tcp service and more http routes

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -27,16 +31,17 @@ var store = []DataStructure{}
 
 func main() {
 	var wg sync.WaitGroup
-
-	wg.Add(2)
+	wg.Add(3)
 
 	http.HandleFunc("/create", createKey)
 	http.HandleFunc("/count", count)
-	http.HandleFunc("/get", getAllKeys)
+	http.HandleFunc("/get", getKey)
+	http.HandleFunc("/", getAllKeys)
+	http.HandleFunc("/delete", deleteKey)
 
 	ticker := time.Tick(time.Second)
 
-	log.Println("-- TEA DB STARTING --")
+	log.Println("-- TEA DB --")
 
 	loadFromDisk()
 
@@ -59,7 +64,127 @@ func main() {
 		http.ListenAndServe("0.0.0.0:8080", nil)
 	}()
 
+	go func() {
+		listener, _ := net.Listen("tcp", ":9000")
+		defer listener.Close()
+		log.Println("Started TCP listening on port 9000")
+		defer wg.Done()
+		for {
+			conn, _ := listener.Accept()
+			go handleConnection(conn)
+		}
+	}()
+
 	wg.Wait()
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Fprintf(conn, "Connected to TeaDB \n")
+	log.Printf("Client %s connected \n", conn.RemoteAddr())
+
+	reader := bufio.NewReader(conn)
+
+	// fmt.Fprintf(conn, "Username: ")
+	// username, _ := reader.ReadString('\n')
+	// username = strings.TrimRight(username, "\n")
+	// fmt.Fprintf(conn, "Password: ")
+	// password, _ := reader.ReadString('\n')
+	// password = strings.TrimRight(password, "\n")
+
+	// if username != "tea" || password != "tea" {
+	// 	fmt.Fprintf(conn, "Invalid username or password\n")
+	// 	conn.Close()
+	// 	return
+	// }
+
+	for {
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("Client %s disconnected", conn.RemoteAddr())
+			} else {
+				log.Println("Error reading command:", err)
+			}
+			conn.Close()
+			return
+		}
+
+		cmd = strings.TrimRight(cmd, "\n")
+		fields := strings.Fields(cmd)
+
+		if fields[0] == "SET" {
+			key := fields[1]
+			value := fields[2]
+			ttl, err := strconv.Atoi(fields[3])
+			if err != nil {
+				fmt.Fprintf(conn, "Invalid ttl format: %s", err)
+				return
+			}
+			for _, ds := range store {
+				if ds.Key == key {
+					fmt.Fprintf(conn, "Key %s already exists\n", key)
+					return
+				}
+			}
+
+			store = append(store, DataStructure{
+				Key:   key,
+				Value: value,
+				TTL:   time.Now().Add(time.Duration(ttl) * time.Second),
+			})
+
+			fmt.Fprintf(conn, "Key: %s Value: %s with TTL of %d seconds added to store \n", key, value, ttl)
+		} else if fields[0] == "GET" {
+			key := fields[1]
+
+			for _, ds := range store {
+				if ds.Key == key {
+					valueBytes, err := json.Marshal(ds.Value)
+					if err != nil {
+						fmt.Fprintf(conn, "No key called %s found\n", key)
+
+						return
+					}
+
+					fmt.Fprintf(conn, " %s \n", valueBytes)
+
+				}
+			}
+
+		} else {
+			fmt.Fprintf(conn, "Invalid command: %s\n", cmd)
+		}
+
+	}
+}
+
+func getKey(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			http.Error(w, "Key not provided", http.StatusBadRequest)
+			return
+		}
+
+		for _, ds := range store {
+			if ds.Key == key {
+				valueBytes, err := json.Marshal(ds.Value)
+				if err != nil {
+					http.Error(w, "Error marshalling value", http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(valueBytes)
+				return
+			}
+		}
+
+		http.Error(w, "Key not found", http.StatusNotFound)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
 
 func getAllKeys(w http.ResponseWriter, r *http.Request) {
@@ -75,6 +200,39 @@ func getAllKeys(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
+}
+
+func deleteKey(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "DELETE" {
+		key := r.URL.Query().Get("key")
+
+		if key == "" {
+			http.Error(w, "Key parameter is missing", http.StatusBadRequest)
+			return
+		}
+
+		var index int
+		var found bool
+		for i, ds := range store {
+			if ds.Key == key {
+				index = i
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			http.Error(w, "Key was not found", http.StatusNotFound)
+			return
+		}
+
+		store = append(store[:index], store[index+1:]...)
+
+		w.Write([]byte("Key was deleted successfully"))
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+
 }
 
 func count(w http.ResponseWriter, r *http.Request) {
@@ -167,6 +325,14 @@ func saveToDisk(store []DataStructure) {
 }
 
 func loadFromDisk() {
+	if _, err := os.Stat("store.json"); os.IsNotExist(err) {
+		file, err := os.Create("store.json")
+		if err != nil {
+			log.Fatalln("Error creating store.json:", err)
+		}
+		file.Close()
+	}
+
 	dataBytes, err := os.ReadFile("store.json")
 	log.Println("Loading data from store.json")
 	if err != nil {
@@ -176,6 +342,6 @@ func loadFromDisk() {
 
 	err = json.Unmarshal(dataBytes, &store)
 	if err != nil {
-		fmt.Println("Error unmarshalling store data:", err)
+		log.Fatalln("Error unmarshalling store data:", err)
 	}
 }

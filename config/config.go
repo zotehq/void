86cc0bcbcc049ebf4c0504b8c563e47d3178225e
duccybaka <usr.bin.ducc.2@gmail.com>
2024-04-15 05:33:53 +0300

    feat(deprecate): migrate from go to rust

--- config/config.go
+++
@@ -1,6 +0,0 @@
-package config
-
-import "github.com/krytonitehq/void/structs"
-
-var Store = []structs.DataStructure{}
-var Config structs.Config
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/krytonitehq/void
-
-go 1.21.8

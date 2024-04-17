189afa032a9215acf3a81bbdec9c0956baf9a5a3
duccybaka <usr.bin.ducc.2@gmail.com>
2024-04-17 21:21:31 +0300

    refactor(conn_handler.rs): remove useless results

--- src/conn_handler.rs
+++ src/conn_handler.rs
@@ -1,21 +1,21 @@
 use crate::logger;
 use may::net::TcpStream;
-use std::io::{Read, Result, Write};
+use std::io::{Read, Write};
 
-pub fn handle_connection(mut stream: TcpStream) -> Result<()> {
+pub fn handle_connection(mut stream: TcpStream) {
   loop {
     let mut buf: Vec<u8> = vec![0; 1024];
 
     let bytes_read = match stream.read(&mut buf) {
       Ok(0) => {
         logger::info("Connection closed");
-        return Ok(());
+        return;
       }
       Ok(n) => n,
       Err(e) => {
         logger::warn("Connection error");
         logger::trace(&e.to_string(), "conn_handler::handle_connection");
-        return Err(e);
+        return;
       }
     };
 
@@ -26,7 +26,7 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<()> {
       Err(e) => {
         logger::warn("Connection error");
         logger::trace(&e.to_string(), "conn_handler::handle_connection");
-        return Err(e);
+        return;
       }
     }
   }

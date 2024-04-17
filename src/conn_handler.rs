1897bd102005f46526cbd15daee4e22835f2101c
duccybaka <usr.bin.ducc.2@gmail.com>
2024-04-17 19:11:23 +0300

    feat: slightly working conn_handler

--- src/conn_handler.rs
+++ src/conn_handler.rs
@@ -1,6 +1,39 @@
 use crate::logger;
-use std::net::TcpStream;
+use std::{
+  io::{BufReader, BufWriter, Read, Write},
+  net::TcpStream,
+};
 
-pub fn handle_connection(mut stream: TcpStream) {
-  loop {}
+// to-do: fix reads, it reads 0 bytes
+pub fn handle_connection(stream: TcpStream) {
+  let mut reader = BufReader::new(&stream);
+  let mut writer = BufWriter::new(&stream);
+
+  loop {
+    let mut buf: Vec<u8> = vec![0; 1024];
+
+    let bytes_read = match reader.read(&mut buf) {
+      Ok(0) => {
+        logger::info("Connection closed");
+        return;
+      }
+      Ok(n) => n,
+      Err(e) => {
+        logger::warn("Connection error");
+        logger::trace(&e.to_string(), "conn_handler::handle_connection");
+        return;
+      }
+    };
+
+    match writer.write_all(&buf[..bytes_read]) {
+      Ok(_) => {
+        continue;
+      }
+      Err(e) => {
+        logger::warn("Connection error");
+        logger::trace(&e.to_string(), "conn_handler::handle_connection");
+        return;
+      }
+    }
+  }
 }
